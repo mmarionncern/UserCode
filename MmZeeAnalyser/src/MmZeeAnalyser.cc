f76a7f625d7a315f6d78493cfc1a3d22edbb28c9
mmarionn <>
2008-11-19 15:13:08 +0000

    *** empty log message ***

---
+++ MmZeeAnalyser/src/MmZeeAnalyser.cc
@@ -0,0 +1,146 @@
+// -*- C++ -*-
+//
+// Package:    MmZeeAnalyser
+// Class:      MmZeeAnalyser
+// 
+/**\class MmZeeAnalyser MmZeeAnalyser.cc mmarionn/MmZeeAnalyser/src/MmZeeAnalyser.cc
+
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Matthieu Marionneau
+//         Created:  Mon Nov 10 14:59:45 CET 2008
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+//#include "DataFormats/TrackReco/interface/Track.h"
+//#include "DataFormats/TrackReco/interface/TrackFwd.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "DataFormats/EgammaReco/interface/BasicClusterFwd.h"
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/EcalDetId/interface/EcalSubdetector.h"
+#include "DataFormats/FWLite/interface/Event.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "../interface/MmZeeAnalyser.h"
+
+using namespace edm;
+using namespace std;
+using namespace cms;
+
+
+// Constructors
+
+MmZeeAnalyser::MmZeeAnalyser(const edm::ParameterSet& iConfig):
+
+  //trackTags_(iConfig.getUntrackedParameter<edm::InputTag>("tracks")),
+  electronCollection_(iConfig.getUntrackedParameter<edm::InputTag>("gsfElectrons"))
+{
+   //now do what ever initialization is needed
+
+}
+
+// Destructors
+
+MmZeeAnalyser::~MmZeeAnalyser()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to for each event  ------------
+void
+MmZeeAnalyser::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+
+   
+   //Electron Recognition
+   
+   cout << "analyzing new event " <<endl;
+   
+   using reco::GsfElectronCollection;
+   
+   edm::Handle<GsfElectronCollection> gsfElectrons;
+   iEvent.getByLabel(electronCollection_, gsfElectrons);
+   edm::LogInfo("")<<"\n\n =================> Treating event "<<iEvent.id()<<" Number of electrons "<<gsfElectrons.product()->size();
+   
+   
+   
+   for(GsfElectronCollection::const_iterator itElec = gsfElectrons->begin();
+       itElec != gsfElectrons->end();
+       itElec++) 
+     {
+       
+       cout<<" test "<<itElec->isElectron()<<"  "<<itElec->gsfTrack().index() <<endl;
+       
+       
+       
+     }
+   
+
+
+
+  /*
+#ifdef THIS_IS_AN_EVENT_EXAMPLE
+   Handle<ExampleData> pIn;
+   iEvent.getByLabel("example",pIn);
+#endif
+   
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+#endif
+  */
+
+
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+MmZeeAnalyser::beginJob(const edm::EventSetup&)
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+MmZeeAnalyser::endJob() {
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(MmZeeAnalyser);
