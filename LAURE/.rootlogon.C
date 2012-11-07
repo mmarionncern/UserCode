2131c64471c72458a604b1489c33bd6a5fbc5f29
mmarionn <>
2012-11-07 14:03:09 +0000

    add rootlogon.C

---
+++ LAURE/.rootlogon.C
@@ -0,0 +1,12 @@
+{
+  //#include <iostream>
+#include <stdlib.h> 
+#include <string>
+#include <map>
+
+  cout<<" Loading libUseTree.so "<<endl;
+  gSystem->Load("$UseTree/lib/libUseTree.so");
+
+   bool Recompute=1;
+   bool* pr=&Recompute;
+}
